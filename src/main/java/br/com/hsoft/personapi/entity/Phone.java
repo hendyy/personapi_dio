92f22ecbfe178ce3f619ec5a7878cc6ed6de44ac
Hendy T Yabiku <hendyy@gmail.com>
2021-07-23 22:27:18 -0300

    Implementacao POST

---
+++ src/main/java/br/com/hsoft/personapi/entity/Phone.java
@@ -0,0 +1,29 @@
+package br.com.hsoft.personapi.entity;
+
+import br.com.hsoft.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false,unique = true)
+    private String number;
+
+}
